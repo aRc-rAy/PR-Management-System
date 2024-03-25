1cdcbcd5fa4340e21796cfc797a00da5123e12c0
Dilip Kumar Yadav <codingiitiandilip@gmail.com>
2024-03-25 13:43:00 +0530

    updated pr desc

--- frontend/src/pages/UserProfile/user.js
+++ frontend/src/pages/UserProfile/user.js
@@ -7,18 +7,22 @@ import "./user.css";
 const User = () => {
 	const { userId } = useParams();
 	const [approvals, setApprovals] = useState([]);
-	const [myPullRequests, setMyPullRequests] = useState([]);
+	const [allApprovals, setAllApprovals] = useState([]);
+	const [status, setStatus] = useState(0);
 	const [user, setUser] = useState({});
+	const [users, setUsers] = useState([]);
 
 	useEffect(() => {
 		const fetchApprovals = async () => {
 			try {
 				const { data } = await api.getApprovals();
+				setAllApprovals(data);
 				const userData = await api.getUser(userId);
+				const allUsers = await api.getUsers();
 				const myApprovalData = await data.filter(
-					(item) => item.approverId === userId
+					(item) => item.approver._id === userId
 				);
-
+				setUsers(allUsers.data);
 				setApprovals(myApprovalData);
 				setUser(userData.data);
 			} catch (error) {
@@ -27,7 +31,7 @@ const User = () => {
 			}
 		};
 		fetchApprovals();
-	}, [userId]);
+	}, [setApprovals, status]);
 
 	const updateStatus = async (id, status) => {
 		try {
@@ -35,6 +39,7 @@ const User = () => {
 			const updatedApprovals = approvals.map((item) =>
 				item._id === id ? { ...item, status } : item
 			);
+			setStatus(!status);
 			setApprovals(updatedApprovals);
 		} catch (error) {
 			console.error(error);
@@ -48,50 +53,99 @@ const User = () => {
 				<h2>Email : {user?.email}</h2>
 				<h2>Notifications: {approvals.length} </h2>
 			</div>
-			<div className="info-box">
-				<div className="left-box">
-					<h2>My PRs : </h2>
-					{myPullRequests.map((item, index) => (
-						<div className="review-box" key={index}>
-							<h3 className="pull-id">
-								<strong>Created by:</strong> {item.pullRequestId}
-							</h3>
-							<h3 className="item-id">
-								<strong>Item id:</strong> {item._id}
-							</h3>
-
-							<h3 className="item-status">
-								<strong>Status :</strong> {item.status}
-							</h3>
-							<div className="status-button">
-								<div className="approve">
-									<button onClick={() => updateStatus(item._id, "Approved")}>
-										Approve
-									</button>
-								</div>
-								<div className="approve">
-									<button onClick={() => updateStatus(item._id, "Rejected")}>
-										Reject
-									</button>
-								</div>
-							</div>
-						</div>
-					))}
-				</div>
-				<div className="right-box">
-					<h2>My Reviews :</h2>
+			<div className="info-box" style={{ display: "flex" }}>
+				<div
+					className="right-box"
+					style={{
+						display: "flex",
+						flexWrap: "wrap",
+						justifyContent: "center",
+						alignItems: "center",
+						flexDirection: "row",
+					}}
+				>
+					{approvals.length === 0 && <h1>.........</h1>}
 					{approvals.map((item, index) => (
-						<div className="review-box" key={index}>
-							<h3 className="pull-id">
-								<strong>Created by:</strong> {item.pullRequestId}
+						<div
+							className="review-box"
+							style={{
+								border: "1px solid black",
+								borderRadius: "8px",
+								width: "40vw",
+								display: "flex",
+								margin: "15px",
+							}}
+							key={index}
+						>
+							<h3 className="item-status">
+								<strong>PR type:</strong> {item?.pullRequest?.prType}
 							</h3>
-							<h3 className="item-id">
-								<strong>Item id:</strong> {item._id}
+							<h3 className="item-status">
+								<strong>PR title:</strong> {item?.pullRequest?.title}
+							</h3>
+							<h3 className="item-status">
+								<strong>PR description:</strong>{" "}
+								{item?.pullRequest?.description}
+							</h3>
+							<h3 className="item-status">
+								<strong>PR status:</strong> {item?.pullRequest?.status}
 							</h3>
 
-							<h3 className="item-status">
-								<strong>Status :</strong> {item.status}
+							<h3 className="item-status" style={{ color: "red" }}>
+								<strong>Status :</strong> {item?.status}
 							</h3>
+
+							<strong style={{ margin: "15px" }}>All approvers :</strong>
+
+							{item?.pullRequest?.approvers.map((mail, index) => (
+								<strong
+									style={{
+										marginLeft: "15px",
+										marginTop: "5px",
+										marginBottom: "5px",
+										display: "block",
+										color: "black",
+									}}
+									key={index}
+								>
+									{index + 1}. {mail.approverId}
+								</strong>
+							))}
+
+							<strong style={{ margin: "15px" }}>Assigned approvers :</strong>
+							{item?.pullRequest?.approvers.map((mail, index) => (
+								<div key={index}>
+									<h3 className="item-status" style={{ color: "red" }}>
+										{allApprovals
+											.filter((approval) => {
+												return (
+													approval?.approver?.email === mail?.approverId &&
+													approval?.pullRequest?._id === item?.pullRequest?._id
+												);
+											})
+											.map((approve, Iindex) => (
+												<div
+													key={Iindex}
+													style={{
+														border: "2px solid black",
+														borderRadius: "8px",
+														margin: "10px",
+														padding: "10px",
+													}}
+												>
+													<h3 className="item-status" style={{ color: "red" }}>
+														<strong> Approver :</strong>{" "}
+														{approve?.approver?.email}
+													</h3>
+													<h3 className="item-status" style={{ color: "red" }}>
+														<strong>Status :</strong> {approve?.status}
+													</h3>
+												</div>
+											))}
+									</h3>
+								</div>
+							))}
+
 							<div className="status-button">
 								<div className="approve">
 									<button onClick={() => updateStatus(item._id, "Approved")}>
